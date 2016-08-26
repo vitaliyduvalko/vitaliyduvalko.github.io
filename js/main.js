ba9c2c274a80e23e6a2bebeae8ba74fc5ad50243
Vitaldo <vitaliy.duvalko@gmail.com>
2016-08-26 15:46:53 +0300

    when i write many functions

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ window.onload = function() {
 			for (var i=-4;i<14;i++) {
 				arr[i]=[];
 				for (var j=-4;j<14;j++) {
-					
+
 					arr[i][j] = {
 						chip: false,
 						shooted: false,
@@ -28,7 +28,7 @@ function makeFirstDeck() {
 	x = Math.round(Math.random()*9);
 	y = Math.round(Math.random()*9);
 	// console.log("x: "+x+"; y: "+y);
-	
+
 	if (arr[x][y].chip==true || arr[x][y+1].chip==true || arr[x][y-1].chip==true ||
 		arr[x-1][y].chip==true || arr[x-1][y+1].chip==true || arr[x-1][y-1].chip==true ||
 		arr[x+1][y].chip==true || arr[x+1][y+1].chip==true || arr[x+1][y-1].chip==true) {
@@ -40,7 +40,7 @@ function makeFirstDeck() {
 
 
 function makeShips(decks,ships) {
-	
+
 	makeFirstDeck();
 	if (dir == 0) { makeGorDecks(decks,ships);} else { makeVertDecks(decks,ships);}
 
@@ -56,7 +56,7 @@ function makeGorDecks(decks,ships) {
 		}
 		return true;
 	}
-	
+
 	(function buildGorShip(decks) {
 
 		if (checkGorDirect(decks,ships)==true) {
@@ -77,7 +77,7 @@ function makeGorDecks(decks,ships) {
 
 
 		} else { makeShips(decks,ships) }
-		console.log(ships);
+
 	})(decks);
 
 };
@@ -108,7 +108,7 @@ function makeVertDecks(decks,ships) {
 		if (checkVertDirect(decks,ships)==true) {
 			for (var i=0;i<decks;i++) {
 				arr[x+i][y].chip=true;
-				
+
 				ships.arr_decks.push(arr[x+i][y]);
 			}
 
@@ -121,12 +121,10 @@ function makeVertDecks(decks,ships) {
 			}
 
 		} else { makeShips(decks,ships) }
-		console.log(ships);
 
-		
-		
+
 	})(decks,ships);
-	
+
 };
 
 makeShips(4,_4decks );
@@ -157,27 +155,24 @@ var array = [];
 		for (var j=0;j<10;j++) {
 			trs[i].children[j].id = i+""+j;
 			if (elem=="myShips") {
-			// 	console.log("Hollo")
-			// 	trs[i].children[j].disabled = true;
-			// 	trs[i].disabled = true;
-			trs[i].style.pointerEvents = 'none';
-		}
-	}
-}
-if (document.querySelector("#"+elem).id=="myShips") {
-	for (var i=0;i<10;i++) {
-		for (var j=0;j<10;j++) {
-			if (array[i][j].chip==true) {
-				trs[i].children[j].style.backgroundColor="#51AFE1"
+				trs[i].style.pointerEvents = 'none';
 			}
 		}
 	}
-}
+	if (document.querySelector("#"+elem).id=="myShips") {
+		for (var i=0;i<10;i++) {
+			for (var j=0;j<10;j++) {
+				if (array[i][j].chip==true) {
+					trs[i].children[j].style.backgroundColor="#51AFE1"
+				}
+			}
+		}
+	}
 })();
 var lastShoots = [];
-var lastComputerShoots = [];
+
 (function bindObjectsСomputerShips() {
-	
+
 	var trsMy = document.querySelector("#myShips").children[0].children;
 	var trsComputer = document.querySelector("#computerShips").children[0].children;
 	var trs = document.querySelector("#"+elem).children[0].children;
@@ -195,12 +190,9 @@ var lastComputerShoots = [];
 			trs[i].children[j].addEventListener("click", check);
 			function check() {
 				if (elem=="computerShips") {
-
 					lastShoots.push(this.obj.chip);
-					// console.log(lastShoots[lastShoots.length-1]);
-					// console.log(lastShoots);
-					
 				}
+
 				if (this.obj.chip == true && this.obj.shooted == true) {
 
 					this.style.backgroundColor = "red";
@@ -254,7 +246,7 @@ var lastComputerShoots = [];
 			function checkShooting() {
 				for (var c = 0; c<10;c++) {
 					for (var k=0; k<10; k++) {
-						(function(c,k) { 
+						(function(c,k) {
 								// console.log(c,k);
 								if (trs[c].children[k].obj.shooted==true) {
 									// console.log(trs[c].children[k])
@@ -267,41 +259,193 @@ var lastComputerShoots = [];
 			}
 		}
 
+
 		if (elem=="computerShips") {
 			var trsForShoot = document.querySelector("#myShips").children[0].children;
-			console.log(trsForShoot);
-			for (var k=0;k<10;k++) {
 
-				for (var j=0;j<10;j++) {
+			for (var x=0;x<10;x++) {
+
+				for (var y=0;y<10;y++) {
 					var shoot;
-					
+					var k,f,k2,f2,lastBoom,lastFunk;
+
+
 					shoot = function () {
+						console.log("lastBoom",lastBoom,k2,f2);
+
+						k = Math.round(Math.random(1,0)*9);
+						f = Math.round(Math.random(1,0)*9);
+
+						function rightShoot(b,c) {
+							console.log("r");
+							++c;
+							if ((trsForShoot[b].children[c])) {
+								console.log(trsForShoot[b].children[c]);
+								if (trsForShoot[b].children[c].obj.shooted == false && trsForShoot[b].children[c].obj.chip == true) {
+									trsForShoot[b].children[c].click();
+									alert("toright");
+									rightShoot(b,c);
+								}
+								else if (trsForShoot[b].children[c].obj.shooted == false && trsForShoot[b].children[c].obj.chip == false) {
+									lastFunk = "right";
+									trsForShoot[b].children[c].click();
+								}
+								else if (trsForShoot[b].children[c].obj.shooted == true) {
+									// alert("cell is shooted");
+									leftShoot(k,f);
+								}
+
+
+							} else {
+								alert("the cell is absence",b,c);
+								leftShoot(k,f);
+							}
+						}
+
+
+						function leftShoot(b,c) {
+							console.log("l");
+							--c;
+							if ((trsForShoot[b].children[c])) {
+								console.log(trsForShoot[b].children[c]);
+								if (trsForShoot[b].children[c].obj.shooted == false && trsForShoot[b].children[c].obj.chip == true) {
+									trsForShoot[b].children[c].click();
+									alert("toleft");
+									leftShoot(b,c);
+								}
+								else if (trsForShoot[b].children[c].obj.shooted == false && trsForShoot[b].children[c].obj.chip == false) {
+									lastFunk = "left";
+									trsForShoot[b].children[c].click();
+								}
+								else if (trsForShoot[b].children[c].obj.shooted == true) {
+									// alert("cell is shooted");
+									downShoot(k,f);
+								}
+
+
+							} else {
+								alert("the cell is absence",b,c);
+								downShoot(k,f)
+
+							}
+						}
+
+
+						function downShoot(b,c) {
+							console.log("d");
+							++b;
+							if ((trsForShoot[b])) {
+								console.log(trsForShoot[b].children[c]);
+								if (trsForShoot[b].children[c].obj.shooted == false && trsForShoot[b].children[c].obj.chip == true) {
+									trsForShoot[b].children[c].click();
+									alert("todown");
+									downShoot(b,c);
+								}
+								else if (trsForShoot[b].children[c].obj.shooted == false && trsForShoot[b].children[c].obj.chip == false) {
+									lastFunk = "down";
+									trsForShoot[b].children[c].click();
+								}
+								else if (trsForShoot[b].children[c].obj.shooted == true) {
+									// alert("cell is shooted");
+									upShoot(k,f);
+								}
+
+
+							} else {
+								alert("the cell is absence",b,c);
+								upShoot(k,f);
+							}
+						}
+
+
+						function upShoot(b,c) {
+							console.log("u");
+							--b;
+							if ((trsForShoot[b])) {
+								console.log(trsForShoot[b].children[c]);
+								if (trsForShoot[b].children[c].obj.shooted == false && trsForShoot[b].children[c].obj.chip == true) {
+									trsForShoot[b].children[c].click();
+									alert("toup");
+									upShoot(b,c);
+								}
+								else if (trsForShoot[b].children[c].obj.shooted == false && trsForShoot[b].children[c].obj.chip == false) {
+									lastFunk = "up";
+									trsForShoot[b].children[c].click();
+
+								}
+								else if (trsForShoot[b].children[c].obj.shooted == true) {
+									alert("cell is shooted");
+									lastBoom==false;
+									// k2 = k;
+									// f2 = f;
+									shoot();
+								}
+
+
+							} else {
+								alert("the cell is absence",b,c);
+								lastBoom==false;
+								// k2 = k;
+								// f2 = f;
+								shoot();
+							}
+						}
+
+
+
 						if (lastShoots[lastShoots.length-1]==false) {
 
-							var k = Math.round(Math.random(1,0)*9);
-							var j = Math.round(Math.random(1,0)*9);
-							if (trsForShoot[k].children[j].obj.shooted == true) {
+
+							if (lastBoom == true && (lastFunk==undefined || lastFunk == "right")) {
+								alert("from",k2,f2);
+								leftShoot(k2,f2);
+							}
+							else if (lastBoom == true && lastFunk=="left") {
+								downShoot(k2,f2);
+							}
+							else if (lastBoom == true && lastFunk=="down") {
+								upShoot(k2,f2);
+							}
+							else if (lastBoom == true && lastFunk=="up") {
+								lastBoom==false;
 								shoot();
-							} else if (trsForShoot[k].children[j].obj.shooted == false && trsForShoot[k].children[j].obj.chip == true) {
-								// console.log(k,j);
-								trsForShoot[k].children[j].obj.shooted = true;
-								trsForShoot[k].children[j].click();
-								shoot();
+							}
+							else if ( lastBoom == false || lastBoom == undefined ) {
+
+								if (trsForShoot[k].children[f].obj.shooted == true) {
+									// lastBoom = false;
+									shoot();
+								} else if (trsForShoot[k].children[f].obj.shooted == false && trsForShoot[k].children[f].obj.chip == true) {
+
+									lastBoom = true;
+									k2=k;f2=f;
+									trsForShoot[k].children[f].click();
+									rightShoot(k,f);
+								// shoot();
 							} else {
-								trsForShoot[k].children[j].obj.shooted = true;
-								trsForShoot[k].children[j].click();
+
+								trsForShoot[k].children[f].obj.shooted = true;
+
+								trsForShoot[k].children[f].click();
+								lastBoom=false;
+
 							}
 						}
 					}
-					
-					trsComputer[k].children[j].addEventListener("click", shoot);
+
+
+
 
 				}
+
+				trsComputer[x].children[y].addEventListener("click", shoot);
+
 			}
 		}
+	}
 
-	})();
-	
+})();
+
 
 }
 
